d7927df1f66e5c40b91e598941ef47da2882ea74
AlejandroN176 <147194662+AlejandroN176@users.noreply.github.com>
2024-10-30 13:07:33 -0500

    interface implemented

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,24 @@
+class SolidIngredient implements ingredients{
+    private String _name;
+    private int _quantity;
+    public void SolidIngredient(){}
+    public void SolidIngredient(String n, int q){
+        _name = n;
+        _quantity = q;
+    }
+public String getName(){
+    return _name;
+    }
+    public int getQuantity(){
+    return _quantity;
+    }
+
+}
+interface ingredients{
+    String getName();
+    int getQuantity();
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
